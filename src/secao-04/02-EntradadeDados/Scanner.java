2c349d99a68d71c376b8edce4d8e89cc8c3a098f
Karen Camilly <karencamilly829@gmail.com>
2025-05-10 13:07:10 -0300

    Scanner: entrada de dados

--- src/secao-04/02-EntradadeDados/Scanner.java
+++ src/secao-04/02-EntradadeDados/Scanner.java
@@ -41,6 +41,30 @@ public class Scanner {
         System.out.print("Você digitou: " + x + ", " + y + ", " + z + ".");
 
         teclado.close(); // Descarta o objeto do tipo Scanner
+
+
+        /* OUTRO EXEMPLO
+
+
+                Scanner sc = new Scanner(System.in);
+
+                System.out.println("Digite algumas coisas por linha:");
+
+                sc.nextLine(); // O nextLine consome um espaço vazio, por isso estou deixando aqui, no caso de haver um string vazio
+                String s1 = sc.nextLine(); // nextLine aceita dados até a quebra de linha
+                String s2 = sc.nextLine(); // Pode ser um string vazio
+                String s3 = sc.nextLine();
+
+                System.out.println("DADOS DIGITADOS:");
+                System.out.println(s1);
+                System.out.println(s2);
+                System.out.println(s3);
+
+                sc.close();
+            }
+
+        }*/
+
     }
 
 }
