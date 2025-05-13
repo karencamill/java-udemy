82326140cd8872385186ed0ca11d7207fbdc3c31
Karen Camilly <karencamilly829@gmail.com>
2025-05-13 19:21:49 -0300

    Scanner: entrada de dados

--- src/secao-04/02-EntradadeDados/Scanner.java
+++ src/secao-04/02-EntradadeDados/Scanner.java
@@ -36,8 +36,8 @@ public class Scanner {
         // Trabalhando com vários tipos
         System.out.println("Digite uma string, um inteiro e um double por linha:"); // Tudo na mesma linha
         x = teclado.next(); // string
-        y = teclado.nextInt(); // Inteiro
-        z = teclado.nextDouble(); // Double
+        y = teclado.nextInt(); // inteiro
+        z = teclado.nextDouble(); // double
         System.out.print("Você digitou: " + x + ", " + y + ", " + z + ".");
 
         teclado.close(); // Descarta o objeto do tipo Scanner
