2fdc12aa34b1bdc5f339c1f5b5cc4b5227f75b8a
Karen Camilly <karencamilly829@gmail.com>
2025-07-28 16:53:40 -0300

    exercício de fixação

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,8 @@ import static java.util.Locale.US;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Olá Mundo");
-        System.out.println("Karen aqui!!!");
+       // System.out.println("Olá Mundo");
+       // System.out.println("Karen aqui!!!");
+
     }
 }
