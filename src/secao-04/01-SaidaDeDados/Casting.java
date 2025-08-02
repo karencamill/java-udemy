261a44a360446d4079fe6a5a48d52f59a796b314
Karen Camilly <karencamilly829@gmail.com>
2025-08-02 14:32:15 -0300

    Adicionando mais exemplos

--- src/secao-04/01-SaidaDeDados/Casting.java
+++ src/secao-04/01-SaidaDeDados/Casting.java
@@ -1,6 +1,7 @@
 public class Casting {
     public static void main(String[] args) {
 
+        // Exemplo 1
         int a, b; // Valores inteiros
         double resultado; // resultado aceita números com ponto flutuante
         double resultado2;
@@ -13,6 +14,16 @@ public class Casting {
 
         System.out.println(resultado); // => 2.0
         System.out.println(resultado2); // => 2.5
+
+
+        // Exemplo 2
+        double a; // Valor com ponto flutuante
+        int b; // Valor inteiro
+
+        a = 5.0;
+        b = (int) a; // Para converter de double para int tem que usar "Casting"
+
+        System.out.println(b);
 
     }
 }
