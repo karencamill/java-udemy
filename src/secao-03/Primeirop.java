d4a66e91c039965008e8745dc7616c2d6806deae
Karen Camilly <129320903+karencamill@users.noreply.github.com>
2025-08-01 21:11:43 -0300

    Update Primeirop.java

--- src/secao-03/Primeirop.java
+++ src/secao-03/Primeirop.java
@@ -1,6 +1,6 @@
 public class primeirop {
     public static void main(String[] args) {
         System.out.println("Olá Mundo");
-        System.out.println("teste");
+        System.out.println("testando");
     }
 }
