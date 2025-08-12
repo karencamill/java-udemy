8ca1c5115b736f42b0baed1b9e3cf18aad8882c4
Karen Camilly <karencamilly829@gmail.com>
2025-08-12 14:08:03 -0300

    Estrutura Condicional

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,49 +8,7 @@ public class Main {
         // System.out.println("Olá Mundo");
         // System.out.println("Karen aqui!!!");
         
-        Locale.setDefault(US);
-        Scanner sc = new Scanner(System.in);
-
-        // Entrada de dados
-
-        double A, B , C;
-        double delta;
-        double x, x1, x2;
-        System.out.println("Digite o valor de A: ");
-        A = sc.nextDouble();
-        System.out.println("Digite o valor de B: ");
-        B = sc.nextDouble();
-        System.out.println("Digite o valor de C: ");
-        C = sc.nextDouble();
-
-        System.out.println();
-
-        if (A == 0) {
-            System.out.println("Não existe equação do 2º grau, A não pode ser igual a 0.");
-        } else {
-
-            // Achar o valor de delta
-            delta = Math.pow(B, 2) - 4 * A * C;
-            System.out.println("O valor de Delta é : " + delta);
-
-            System.out.println();
-
-            if (delta < 0) {
-                System.out.println("A equação não possui raízes reais");
-            } else if (delta == 0) {
-                x = - B / 2 * A;
-                System.out.println("A equação possui duas raízes iguais: x = " + x);
-            } else {
-                x1 = (-B + Math.sqrt(delta)) / (2 * A);
-                x2 = (-B - Math.sqrt(delta)) / (2 * A);
-                System.out.println("A equação possui duas raízes reais:");
-                System.out.println("x1 = " + x1);
-                System.out.println("x2 = " + x2);
-
-            }
-        }
-
-        sc.close();
-        
+       Scanner sc = new Scanner(System.in);
+       sc.close();
     }
 }
---
+++ src/secao-05/02-EstruturaCondicional/Condicao.java
@@ -0,0 +1,23 @@
+public class Condicao {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int hora;
+
+        System.out.println("Quantas horas:");
+        hora = sc.nextInt();
+
+        if (hora <= 12) {
+            System.out.println("Bom dia");
+        }
+        else if (hora > 12 && hora < 18) {
+            System.out.println("Boa tarde");
+        }
+        else { // else não tem condição.
+            System.out.println("Boa noite");
+        }
+
+        sc.close();
+    }
+}
