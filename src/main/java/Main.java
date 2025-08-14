3588e1c10362f7fde1e154cc09c662f30a94f19a
Karen Camilly <karencamilly829@gmail.com>
2025-08-14 00:52:49 -0300

    Lista de Exercicios

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,25 +8,5 @@ public class Main {
         // System.out.println("Olá Mundo");
         // System.out.println("Karen aqui!!!");
 
-        Locale.setDefault(US);
-        Scanner sc = new Scanner(System.in);
-
-        double num = sc.nextDouble();
-
-        if (num >= 0.0 && num <= 25.0){
-            System.out.println("Intervalo (0,25)");
-        } 
-        else if (num > 25.0 && num <= 50.0) {
-            System.out.println("Intervalo (25,50)");
-        }
-        else if (num > 50.0 && num <= 75.0) {
-            System.out.println("Intervalo (50,75)");
-        } else if (num > 75.0 && num <= 100.0) {
-            System.out.println("Intervalo (75,100)");
-        } else {
-            System.out.println("Fora de Intervalo");
-        }
-
-        sc.close();
     }
 }
--- src/secao-05/Exercicios1/Exe.java
+++ src/secao-05/Exercicios1/Exe.java
@@ -7,6 +7,52 @@ public class Exe {
     public static void main(String[] args) {
 
 
+        /*TESTE*/
+        Locale.setDefault(US);
+        Scanner sc = new Scanner(System.in);
+
+        // Entrada de dados
+        double A, B , C;
+        double delta;
+        double x, x1, x2;
+        System.out.println("Digite o valor de A: ");
+        A = sc.nextDouble();
+        System.out.println("Digite o valor de B: ");
+        B = sc.nextDouble();
+        System.out.println("Digite o valor de C: ");
+        C = sc.nextDouble();
+
+        System.out.println();
+
+        if (A == 0) {
+            System.out.println("Não existe equação do 2º grau, A não pode ser igual a 0.");
+        } else {
+
+            // Achar o valor de delta
+            delta = Math.pow(B, 2) - 4 * A * C;
+            System.out.println("O valor de Delta é : " + delta);
+
+            System.out.println();
+
+            if (delta < 0) {
+                System.out.println("A equação não possui raízes reais");
+            } else if (delta == 0) {
+                x = - B / 2 * A;
+                System.out.println("A equação possui duas raízes iguais: x = " + x);
+            } else {
+                x1 = (-B + Math.sqrt(delta)) / (2 * A);
+                x2 = (-B - Math.sqrt(delta)) / (2 * A);
+                System.out.println("A equação possui duas raízes reais:");
+                System.out.println("x1 = " + x1);
+                System.out.println("x2 = " + x2);
+
+            }
+
+        }
+
+        sc.close();
+
+
         /*Exercio 1
         Fazer um programa para ler um número inteiro, e depois dizer se este número é negativo ou não.*/
         Scanner sc = new Scanner(System.in);
@@ -121,7 +167,6 @@ public class Exe {
         sc.close();
 
 
-
         /*Exercicio 6
         Você deve fazer um programa que leia um valor qualquer e apresente uma mensagem dizendo em qual dos seguintes
         intervalos ([0,25], (25,50], (50,75], (75,100]) este valor se encontra. Obviamente se o valor não estiver em
@@ -155,7 +200,73 @@ public class Exe {
 
         Se o ponto estiver na origem, escreva a mensagem “Origem”.
         Se o ponto estiver sobre um dos eixos escreva “Eixo X” ou “Eixo Y”, conforme for a situação.*/
+        Locale.setDefault(US);
+        Scanner sc = new Scanner(System.in);
 
+        double x, y;
+        x = sc.nextDouble();
+        y = sc.nextDouble();
+
+        if (x > 0 && y > 0){
+            System.out.println("Q1");
+        }
+        else if (x < 0.0 && y > 0.0) {
+            System.out.println("Q2");
+        }
+        else if (x < 0.0 && y < 0.0) {
+            System.out.println("Q3");
+        }
+        else if (x > 0.0 && y < 0.0) {
+            System.out.println("Q4");
+        }
+        else if (x == 0.0 && y == 0.0) {
+            System.out.println("Origem");
+        } else if (x == 0) {
+            System.out.println("Eixo y");
+        }else {
+            System.out.println("Eixo x");
+        }
+
+        sc.close();
+
+
+        /*Exercicio 8
+       Em um país imaginário denominado Lisarb, todos os habitantes ficam felizes em pagar seus impostos, pois sabem
+       que nele não existem políticos corruptos e os recursos arrecadados são utilizados em benefício da população, sem
+       qualquer desvio. A moeda deste país é o Rombus, cujo símbolo é o R$.
+
+       Leia um valor com duas casas decimais, equivalente ao salário de uma pessoa de Lisarb. Em seguida, calcule e
+       mostre o valor que esta pessoa deve pagar de Imposto de Renda, segundo a tabela abaixo.
+
+       Lembre que, se o salário for R$ 3002.00, a taxa que incide é de 8% apenas sobre R$ 1000.00, pois a faixa de
+       salário que fica de R$ 0.00 até R$ 2000.00 é isenta de Imposto de Renda. No exemplo fornecido (abaixo), a taxa é
+       de 8% sobre R$ 1000.00 + 18% sobre R$ 2.00, o que resulta em R$ 80.36 no total.*/
+
+        Locale.setDefault(US);
+        Scanner sc = new Scanner(System.in);
+
+        double salario;
+        double imposto = 0.0;
+
+        salario = sc.nextDouble();
+
+        if (salario <= 2000.0) {
+            System.out.println("Isento");
+            return;
+        }
+        else if (salario <= 3000.0) {
+            imposto = (salario - 2000.0) * 0.08;
+        }
+        else if (salario <= 4500.0) {
+            imposto = (salario - 3000.0) * 0.18 + 1000.0 * 0.08;
+        }
+        else {
+            imposto = (salario - 4500.0) * 0.28 + 1500.0 * 0.18 + 1000.0 * 0.08;
+        }
+
+        System.out.printf("R$ %.2f", imposto);
+
+        sc.close();
 
     }
 }
