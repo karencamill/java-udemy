0c9cdf31ac24c2b45910b0b24299409cc439a91a
Karen Camilly <karencamilly829@gmail.com>
2025-09-07 19:36:19 -0300

    Exercício

--- src/secao-06/Exercicios2/Exe1.java
+++ src/secao-06/Exercicios2/Exe1.java
@@ -24,8 +24,66 @@ public class Exe1 {
         sc.close();
 
 
+        /*Exercicio 2
+         Escreva um programa para ler as coordenadas (X,Y) de uma quantidade indeterminada de pontos no sistema
+         cartesiano. Para cada ponto escrever o quadrante a que ele pertence. O algoritmo será encerrado quando pelo
+         menos uma de duas coordenadas for NULA (nesta situação sem escrever mensagem alguma).*/
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        while (x != 0 && y !=0) {
+            if (x > 0 && y > 0){
+                System.out.println("primeiro");
+            } else if (x > 0 && y < 0) {
+                System.out.println("quarto");
+            } else if (x < 0 && y < 0) {
+                System.out.println("terceiro");
+            } else {
+                System.out.println("segundo");
+            }
+
+            x = sc.nextInt();
+            y = sc.nextInt();
+        }
+
+        sc.close();
 
 
+        /*Exercicio 3
+        Um Posto de combustíveis deseja determinar qual de seus produtos tem a preferência de seus clientes. Escreva
+        um algoritmo para ler o tipo de combustível abastecido (codificado da seguinte forma: 1.Álcool 2.Gasolina 3.Diesel
+        4.Fim). Caso o usuário informe um código inválido (fora da faixa de 1 a 4) deve ser solicitado um novo código (até
+        que seja válido). O programa será encerrado quando o código informado for o número 4. Deve ser escrito a
+        mensagem: "MUITO OBRIGADO" e a quantidade de clientes que abasteceram cada tipo de combustível, conforme
+        exemplo.*/
+        Scanner sc = new Scanner(System.in);
+
+        int alcool = 0;
+        int gasolina = 0;
+        int diesel = 0;
+
+        int numero = sc.nextInt();
+
+        while (numero != 4) {
+            if(numero == 1){
+                alcool = alcool + 1;
+            } else if (numero == 2) {
+                gasolina = gasolina + 1;
+            }else if (numero == 3){
+                diesel = diesel + 1;
+            }
+
+            numero = sc.nextInt();
+        }
+
+        System.out.println("MUITO OBRIGADO");
+        System.out.println("Alcool: " + alcool);
+        System.out.println("Gasolina: " + gasolina);
+        System.out.println("Diesel: " + diesel);
+
+        sc.close();
 
             }
     }
