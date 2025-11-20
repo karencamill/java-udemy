37cd72878d9a3da5fb4d1a482bbd2b82c96aaf9e
Karen Camilly <karencamilly829@gmail.com>
2025-11-20 16:30:32 -0300

    Exercício

--- src/secao-06/Exercicios2/Exe2.java
+++ src/secao-06/Exercicios2/Exe2.java
@@ -27,6 +27,28 @@ public class Exe2 {
         Mostre quantos destes valores X estão dentro do intervalo [10,20] e quantos estão fora do intervalo, mostrando
         essas informações conforme exemplo (use a palavra "in" para dentro do intervalo, e "out" para fora do intervalo).
         */
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int in = 0;
+        int out = 0;
+
+        for (int i=0; i<n; i++){
+            int x = sc.nextInt();
+            if (x >= 10 && x <= 20){
+                in = in + 1;
+            }
+            else {
+                out = out + 1;
+            }
+        }
+
+        System.out.println();
+        System.out.println(in + " in");
+        System.out.println(out + " out");
+
+        sc.close();
 
 
     }
