e8b6b8738812756295595c3cd50dc8cd355bb3fa
Karen Camilly <karencamilly829@gmail.com>
2025-12-01 19:54:55 -0300

    Estrutura Repetitiva

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,21 +11,16 @@ public class Main {
                 Locale.setDefault(US);
                 Scanner sc = new Scanner(System.in);
 
-                System.out.print("Digite a temperatura em Celsius: ");
-                double C = sc.nextDouble();
-                double F = 9.0 * C / 5.0 + 32.0;
-                System.out.printf("Equivalente em Fahrenheit: %.1f%n", F);
-                System.out.print("Deseja repetir (s/n)?");
-                char resp = sc.next().charAt(0);
+                char resp;
 
-                while (resp != 'n') {
+                do {
                     System.out.print("Digite a temperatura em Celsius: ");
-                    C = sc.nextDouble();
-                    F = 9.0 * C / 5.0 + 32.0;
+                    double C = sc.nextDouble();
+                    double F = 9.0 * C / 5.0 + 32.0;
                     System.out.printf("Equivalente em Fahrenheit: %.1f%n", F);
                     System.out.print("Deseja repetir (s/n)? ");
                     resp = sc.next().charAt(0);
-                }
+                } while (resp != 'n');
 
                 sc.close();
             }
--- src/secao-06/EstruturaRepetitivas/Faca.java
+++ src/secao-06/EstruturaRepetitivas/Faca.java
@@ -29,11 +29,40 @@ public class Faca {
 
         sc.close();
 
-        // Melhorado
+        // Mesmo Programa mais com um "jeitinho"
+        Locale.setDefault(US);
+        Scanner sc = new Scanner(System.in);
+
+        char resp = 's';
+
+        while (resp != 'n') {
+            System.out.print("Digite a temperatura em Celsius: ");
+            double C = sc.nextDouble();
+            double F = 9.0 * C / 5.0 + 32.0;
+            System.out.printf("Equivalente em Fahrenheit: %.1f%n", F);
+            System.out.print("Deseja repetir (s/n)? ");
+            resp = sc.next().charAt(0);
+        }
+
+        sc.close();
 
 
+        // Usando FAÇA-ENQUANTO
+        Locale.setDefault(US);
+        Scanner sc = new Scanner(System.in);
 
+        char resp;
 
+        do {
+            System.out.print("Digite a temperatura em Celsius: ");
+            double C = sc.nextDouble();
+            double F = 9.0 * C / 5.0 + 32.0;
+            System.out.printf("Equivalente em Fahrenheit: %.1f%n", F);
+            System.out.print("Deseja repetir (s/n)? ");
+            resp = sc.next().charAt(0);
+        } while (resp != 'n');
+
+        sc.close();
 
 
             }
