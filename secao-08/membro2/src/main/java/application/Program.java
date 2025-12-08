4b0f28bdd8df69959c6af9bd41b31df0e0f9e387
Karen Camilly <129320903+karencamill@users.noreply.github.com>
2025-12-08 18:45:02 -0300

    Add files via upload

---
+++ secao-08/membro2/src/main/java/application/Program.java
@@ -0,0 +1,32 @@
+package application;
+
+import util.Calculator;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        //Como os membros da classe Calculator não é static eu tenho que instânciar o objeto
+        Calculator calc = new Calculator();
+
+        System.out.print("Enter radius: ");
+        double radius = sc.nextDouble();
+
+        double c = calc.circumference(radius);
+        double v = calc.volume(radius);
+
+        System.out.printf("Circumference: %.2f%n", c);
+        System.out.printf("Volume: %.2f%n", v);
+        System.out.printf("PI value: %.2f%n" , calc.PI);
+
+        sc.close();
+    }
+
+
+}
