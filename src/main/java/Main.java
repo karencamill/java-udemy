b339dbff7529ab92f6793418fb998ee08f4170e0
Karen Camilly <karencamilly829@gmail.com>
2025-12-09 19:18:57 -0300

    Testando uns negocios

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ import static java.util.Locale.US;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Se eu não desistir o que será que acontece?");
-        System.out.println("Eu não desisti ainda porque tem gente esperando eu vencer para vencer também :( ")
+        System.out.println("Eu não desisti ainda porque tem gente esperando eu vencer para vencer também :( ");
     }
 }
 
