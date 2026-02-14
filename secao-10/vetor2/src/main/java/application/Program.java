5e84e64d45a890d989c02cf0fe50dcad19e0c171
Karen Camilly <129320903+karencamill@users.noreply.github.com>
2026-02-14 15:12:40 -0300

    vetor

---
+++ secao-10/vetor2/src/main/java/application/Program.java
@@ -0,0 +1,35 @@
+package application;
+
+import entities.Product;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        Product[] vect = new Product[n];
+
+        for (int i = 0; i< vect.length; i++) {
+            sc.nextLine();
+            String name = sc.nextLine();
+            double price = sc.nextDouble();
+            vect[i] = new Product(name, price);
+        }
+
+        double soma = 0.0;
+        for (int i=0; i< vect.length; i++) {
+            soma += vect[i].getPrice(); //getPrice pega apenas o preço
+        }
+        double media = soma / vect.length;
+
+        System.out.printf("AVERAGE PRICE = %.2f%n" , media);
+
+        sc.close();
+
+    }
+}
