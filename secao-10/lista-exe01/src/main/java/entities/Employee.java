fbfd75fb2ef8b6392d9301f42b4cf676d8ae4042
Karen Camilly <karencamilly829@gmail.com>
2026-02-24 09:52:48 -0300

    exercicio

--- secao-10/lista-exe01/src/main/java/entities/Employee.java
+++ secao-10/lista-exe01/src/main/java/entities/Employee.java
@@ -6,9 +6,47 @@ public class Employee {
     private String name;
     private Double salary;
 
-    public void increaseSalary(double percentage){
-        percentage = salary * 0.10;
+    public Employee() {
     }
 
+    public Employee(Integer id, String name, Double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public void increaseSalary(double percentage) {
+        salary = salary + salary * percentage / 100.0;
+    }
+
+    @Override
+    public String toString() {
+        return  id + ", " + name + ", " + String.format("%.2f" , salary);
+    }
 }
+
+
